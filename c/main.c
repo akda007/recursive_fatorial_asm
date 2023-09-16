617006f4f327bcc96dd2f419da7b69b7b885c5b5
Andrey Araujo <65751084+akda007@users.noreply.github.com>
2023-09-16 15:32:38 -0300

    Create main.c

---
+++ c/main.c
@@ -0,0 +1,16 @@
+int fact(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * fact(n - 1)
+  }
+}
+
+int main() {
+  int a;
+  printf("Enter a number: ");
+  scanf("%d", &a);
+  int result = fact(a);
+  printf("The factorial of %d is %d\n", a, result);
+  return 0;
+}
